0caec2e0b3d6e9d0cc8050c44abad28b3b20f5d1
Greg Orvik <gvorvik@gmail.com>
2018-05-22 16:45:32 -0500

    set up the store properly

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import logger from 'redux-logger';
 
-
+const firstReducer = (state = {}, action) => {
+    return state;
+};
 
 const storeInstance = createStore(
 
     combineReducers({
-
+        firstReducer,
     }),
 
     applyMiddleware(logger),
 );
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
