9c7e0189b4320d8d0d70c83c24d6e675a92cd2c3
Greg Orvik <gvorvik@gmail.com>
2018-05-22 16:41:08 -0500

    source in app with store set up

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import {connect} from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const storeInstance = createStore(
+
+    combineReducers({
+
+    }),
+
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
